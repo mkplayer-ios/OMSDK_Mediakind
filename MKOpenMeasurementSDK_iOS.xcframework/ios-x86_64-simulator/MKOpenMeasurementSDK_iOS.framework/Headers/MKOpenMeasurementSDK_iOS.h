3c1881b64e593a757aca321dd32c248ea21b760f
Karthi K <karthi.k@mediakind.com>
2023-09-06 10:10:47 +0530

    Added mediakind omsdk

---
+++ MKOpenMeasurementSDK_iOS.xcframework/ios-x86_64-simulator/MKOpenMeasurementSDK_iOS.framework/Headers/MKOpenMeasurementSDK_iOS.h
@@ -0,0 +1,18 @@
+//
+//  MKOpenMeasurementSDK_iOS.h
+//  MKOpenMeasurementSDK-iOS
+//
+//  Created by Deepak Pagadala on 20/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MKOpenMeasurementSDK_iOS.
+FOUNDATION_EXPORT double MKOpenMeasurementSDK_iOSVersionNumber;
+
+//! Project version string for MKOpenMeasurementSDK_iOS.
+FOUNDATION_EXPORT const unsigned char MKOpenMeasurementSDK_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKOpenMeasurementSDK_iOS/PublicHeader.h>
+
+
